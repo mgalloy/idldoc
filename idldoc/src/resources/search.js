bd7fdff38ba175e4bd58343d8851b2a873b5cecd
mgalloy <mgalloy@116d6c40-d423-0410-b1d3-c737fd712164>
2007-12-17 16:15:47 +0000

    Retrieve and echo search terms.

--- idldoc/src/resources/search.js
+++ idldoc/src/resources/search.js
@@ -1,7 +1,14 @@
 function basicsearch() {
-  alert("Searching...");
+  searchString = document.basicForm.basicText.value;
+  
+  alert("Searching...\n\nSearch terms = " + searchString);
 }
 
 function advancedsearch() {
-  alert("Advanced searching...");
+  routineName = document.advancedForm.routinename.value;
+  comments = document.advancedForm.comments.value;
+  parameters = document.advancedForm.parameters.value;
+  authors = document.advancedForm.authors.value;
+  
+  alert("Advanced searching...\n\nRoutine name = " + routineName + "\nComments = " + comments + "\nParameters = " + parameters + "\nAuthors = " + authors);
 }
